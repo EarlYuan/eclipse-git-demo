dedc48c9af37c5ef9a61e6a599405de6c6e1a885
earl <earlyuan@163.com>
2017-03-05 22:15:47 +0800

    earl1 say hello git

--- src/earl/test/git/HelloGit.java
+++ src/earl/test/git/HelloGit.java
@@ -3,7 +3,7 @@ package earl.test.git;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("hello git");
+		System.out.println("earl1 say£ºhello git");
 		
 	}
 
